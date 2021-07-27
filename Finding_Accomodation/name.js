7f734c769ca7ed74af0b12845e5764384d4473ea
Ashutosh Kamboj <57835919+ASHUTOSH-KAMBOJ@users.noreply.github.com>
2021-07-27 14:41:35 +0530

    Delete Finding_Accomodation directory

--- Finding_Accomodation/name.js
+++
@@ -1,9 +0,0 @@
-function name() {
-	var name=document.getElementById("lname").value;
-	
-	document.getElementById("welcome").innerHTML="Welcome "+name;
-}
-function ShowNo(n)
-	{
-		alert("The Phone no of Owner is: \n\n "+n);
-	}
